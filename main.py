d67a2e623a09e3196ea23a200aab547c119294e7
Cemetary Doll <rakw1995@gmail.com>
2019-08-29 14:13:34 -0400

    completes assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('\033[30;1;40mMy name is \033[31;1;40mRachel Gentry.')
+print('\n\033[30;1;40mMy favorite game of all time is \033[31;1;40mSilent Hill 2.\033[30;1;40m The story, the design, and the gameplay just cannot be beat for me.')
+print('\nI have never done programming before so this is a brand new world for me. My only experience with coding of any kind is\033[31;1;40m HTML and CSS.')
+print('\n\033[30;1;40mI am just generally \033[31;1;40mexcited to be learning \033[30;1;40magain after taking some time off from college.')
+print('\nMy stackoverflow.com user number is \033[31;1;40m11991613.')
+print('\n\033[30;1;40mThe link to my github profile is \033[31;1;40mhttps://github.com/rakw1995\n')
